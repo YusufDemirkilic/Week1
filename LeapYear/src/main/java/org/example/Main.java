cd89f9535527eea912b8f5204638cbd7531fb5f4
Yusuf Demirkılıç <81050891+YusufDemirkilic@users.noreply.github.com>
2024-02-22 22:45:29 +0300

    Update Main.java
    change description

--- LeapYear/src/main/java/org/example/Main.java
+++ LeapYear/src/main/java/org/example/Main.java
@@ -5,14 +5,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // kullanıcıdan veri alndı
         int year;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter year value: ");
         year = scanner.nextInt();
-
+        // yıl 4 ün katı ve 100 e bölümğnden kalan 0 değilse yazdır
         if (year % 4 == 0 && !(year % 100 == 0)) {
 
             System.out.println(year + " leap Year.");
+            // 400 ile bölümden kalan 0 ise yazdır
         } else if (year % 400 == 0) {
 
             System.out.println(year + "  Leap year");
